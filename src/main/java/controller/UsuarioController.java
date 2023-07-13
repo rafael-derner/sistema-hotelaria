f82d3b51d7648644489697e15ffb3a618b457c40
User <User@DESKTOP-I6LIR4C>
2023-07-12 21:18:23 -0300

    Verificador de CPF.

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -39,8 +39,8 @@ public class UsuarioController {
 			mensagemValidacao += "Nome inválido \n";
 		}
 
-		if (novoUsuario.getCpf() == null || novoUsuario.getCpf().trim().length() < 2) {
-			mensagemValidacao += "Telefone inválido \n";
+		if (novoUsuario.getCpf() == null || novoUsuario.getCpf().trim().length() < 2 || !validarCpf(novoUsuario.getCpf())) {
+			mensagemValidacao += "CPF inválido \n";
 		}
 
 		if (novoUsuario.getTelefone() == null || novoUsuario.getTelefone().trim().length() < 2) {
@@ -50,6 +50,55 @@ public class UsuarioController {
 		if (!mensagemValidacao.isEmpty()) {
 			throw new CampoInvalidoException(mensagemValidacao);
 		}
+	}
+
+
+
+	private boolean validarCpf(String cpf) {
+		//Remove caracteres não numéricos
+		cpf = cpf.replaceAll("\\D+", "");
+
+        // Verifica se o CPF possui 11 dígitos
+        if (cpf.length() != 11) {
+            return false;
+        }
+
+        // Verifica se todos os dígitos são iguais
+        if (cpf.matches("(\\d)\\1{10}")) {
+            return false;
+        }
+
+        // Calcula o primeiro dígito verificador
+        int soma = 0;
+        for (int i = 0; i < 9; i++) {
+            soma += Character.getNumericValue(cpf.charAt(i)) * (10 - i);
+        }
+        int primeiroDigito = 11 - (soma % 11);
+        if (primeiroDigito > 9) {
+            primeiroDigito = 0;
+        }
+
+        // Verifica o primeiro dígito verificador
+        if (Character.getNumericValue(cpf.charAt(9)) != primeiroDigito) {
+            return false;
+        }
+
+        // Calcula o segundo dígito verificador
+        soma = 0;
+        for (int i = 0; i < 10; i++) {
+            soma += Character.getNumericValue(cpf.charAt(i)) * (11 - i);
+        }
+        int segundoDigito = 11 - (soma % 11);
+        if (segundoDigito > 9) {
+            segundoDigito = 0;
+        }
+
+        // Verifica o segundo dígito verificador
+        if (Character.getNumericValue(cpf.charAt(10)) != segundoDigito) {
+            return false;
+        }
+
+        return true; // CPF válido
 	}
 
 	public List<Usuario> consultarComFiltro(UsuarioSeletor usuarioSeletor) {
